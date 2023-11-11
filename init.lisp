b565bd23586cb878975e229987e31d0ceedd719d
paku <paku@skyizwhite.dev>
2023-11-12 02:41:34 +0900

    Add slime-qlot-exec command

--- init.lisp
+++ init.lisp
@@ -17,7 +17,7 @@
   "ȣ" 'lem-paredit-mode:paredit-barf)
 
 ;;; Variable config
-(setf *scroll-recenter-p* t)
+(setf *scroll-recenter-p* nil)
 (setf (variable-value 'lem/line-numbers:line-numbers :global) t)
 (setf (variable-value 'lem-lisp-mode/paren-coloring:paren-coloring :global) t)
 
@@ -26,3 +26,8 @@
               (lambda (buffer)
                 (when (eq (buffer-major-mode buffer) 'lem-lisp-mode:lisp-mode)
                   (change-buffer-mode buffer 'lem-paredit-mode:paredit-mode t))))
+
+(define-command slime-qlot-exec (directory) ((prompt-for-directory (format nil "Project directory (~A): " (buffer-directory))))
+  (let ((command (first (lem-lisp-mode/implementation::list-roswell-with-qlot-commands))))
+    (when command
+      (lem-lisp-mode:run-slime command :directory directory))))
