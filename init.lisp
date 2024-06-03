7ad1ddf013d6e9112a726a5bf4c6a45357e50545
paku <paku@skyizwhite.dev>
2024-06-03 17:01:27 +0900

    Revert "Add transparent-decaf theme"
    This reverts commit c72ed8c2c24d3db9d3458c28c6f5720da7ec7098.

--- init.lisp
+++ init.lisp
@@ -5,39 +5,6 @@
 #+lem-sdl2
 (lem-sdl2:set-keyboard-layout :jis)
 
-(define-color-theme "transparent-decaf" ()
-  (:display-background-mode :dark)
-  (:foreground "#cccccc")
-  (:inactive-window-background nil)
-  (region :foreground nil :background "#515151")
-  (syntax-warning-attribute :foreground "#ff7f7b")
-  (syntax-string-attribute :foreground "#beda78")
-  (syntax-comment-attribute :foreground "#777777")
-  (syntax-keyword-attribute :foreground "#efb3f7")
-  (syntax-constant-attribute :foreground "#ffbf70")
-  (syntax-function-name-attribute :foreground "#90bee1")
-  (syntax-variable-attribute :foreground "#ff7f7b")
-  (syntax-type-attribute :foreground "#ffd67c")
-  (syntax-builtin-attribute :foreground "#bed6ff")
-  (:base00 "#2d2d2d")
-  (:base01 "#393939")
-  (:base02 "#515151")
-  (:base03 "#777777")
-  (:base04 "#b4b7b4")
-  (:base05 "#cccccc")
-  (:base06 "#e0e0e0")
-  (:base07 "#ffffff")
-  (:base08 "#ff7f7b")
-  (:base09 "#ffbf70")
-  (:base0a "#ffd67c")
-  (:base0b "#beda78")
-  (:base0c "#bed6ff")
-  (:base0d "#90bee1")
-  (:base0e "#efb3f7")
-  (:base0f "#ff93b3"))
-
-(load-theme "transparent-decaf")
-
 ;;; Key bindings
 ;; workaround
 (define-key *global-keymap* "ȸ" 'scroll-up)         ; C-Up
